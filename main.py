9bf7114adcd950186b7b2c33cfc4d1fd09e4cf79
Nikolai <nvenchuk@mail.ru>
2023-07-31 12:22:17 +0700

    внес изменения

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import random, randrange, randint
 
 # Даны два неупорядоченных набора целых чисел (может быть, с повторениями).
 # Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
@@ -7,9 +7,9 @@ from random import randint
 
 from random import randint
 
-n_set = set(randint(1, 20) for i in range(int(input('Введите количество элементов первого множества: '))))
+n_set = set(randrange(1, 20) for i in range(int(input('Введите количество элементов первого множества: '))))
 print(n_set)
-m_set = set(randint(1, 20) for i in range(int(input('Введите количество элементов второго множества: '))))
+m_set = set(randrange(1, 20) for i in range(int(input('Введите количество элементов второго множества: '))))
 print(m_set)
 s_set = sorted(n_set.intersection(m_set))
 print(s_set)
